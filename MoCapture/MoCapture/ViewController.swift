e7c589faeb573b9be9e8e97fe09681e8d7b30e79
Ezeh Cynthia Ifeoma <goldncyndy@gmail.com>
2024-08-18 16:55:10 +0100

    Initial project commit

---
+++ MoCapture/MoCapture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoCapture
+//
+//  Created by Cynthia D'Phoenix on 8/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
